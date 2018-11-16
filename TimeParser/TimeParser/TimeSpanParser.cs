ac520763cd157a0c518011afb501a03cfd91171a
Peter Halasz <peter@becauseofgames.com>
2018-11-17 09:31:26 +1100

    Move TryParsePrefixed out to its own cs file (partial class)

--- TimeParser/TimeParser/TimeSpanParser.cs
+++ TimeParser/TimeParser/TimeSpanParser.cs
@@ -21,7 +21,7 @@ namespace TimeSpanParserUtil {
         }
     }
 
-    public class TimeSpanParser {
+    public partial class TimeSpanParser {
         public static TimeSpan Parse(string text) {
             // possible exceptions:
             // FormatException
@@ -244,7 +244,7 @@ namespace TimeSpanParserUtil {
                 throw new ArgumentException("Bad default selection.");
             }
 
-            //TODO: https://social.msdn.microsoft.com/Forums/en-US/431d51f9-8003-4c72-ba1f-e830c6ad75ba/regex-to-match-all-number-formats-used-around-the-world?forum=regexp
+            //TODO (or not: overly limited: requires groups of 3 numbers or fails): https://social.msdn.microsoft.com/Forums/en-US/431d51f9-8003-4c72-ba1f-e830c6ad75ba/regex-to-match-all-number-formats-used-around-the-world?forum=regexp
 
             text = text.Normalize(NormalizationForm.FormKC); // fixing any fullwidth characters
             text = text.Replace('_', ' ');
@@ -282,18 +282,21 @@ namespace TimeSpanParserUtil {
             // may change:
             // - starting colon will be ignored, ":30" treated as "30"
             // - but not after: 3: (treated as "3")
+            // - in future, starting-colon numbers may get their own option
 
             //TODO: 
             // - support for https://en.wikipedia.org/wiki/ISO_8601#Durations
             // - better exception messages
-            // - word numbers ("one second")
+            // - word numbers ("one second", "a day")
+            // - Move this list to the Wiki 
 
             //TODO some day:
             // - accept "minutes:30, seconds:10" or "minutes=30"
-            // - accept "5:10 mm:ss" format (number with its format)
+            // - accept "5:10 [mm:ss]" format (number with its format)
             // - accept weird SI units like microsecond, megaseconds, gigaseconds, decaseconds, etc
             // - give error on slash, e.g. 20/sec
             // - explicitly accept weird plurals, e.g. 20 sec/s or second(s) -- already fine already because creates word boundry
+            // - fractional units: 1/2 hour
 
             var numberRegex = new Regex(numberRegexStr); // TODO: re-use regex + RegexOptions.Compiled
             var zeroRegex = new Regex(zeroRegexStr);
@@ -413,83 +416,5 @@ namespace TimeSpanParserUtil {
             return !options.FailOnUnitlessNumber || willSucceed;
         }
 
-        /// <summary>
-        /// Note: a special entries matches["0"] matches["1"] etc are included if `text` starts with timespans.
-        /// </summary>
-        /// <param name="text"></param>
-        /// <param name="uncolonedDefault"></param>
-        /// <param name="colonedDefault"></param>
-        /// <param name="prefixes"></param>
-        /// <param name="matches"></param>
-        /// <returns></returns>
-        public static bool TryParsePrefixed(string text, string[] prefixes, Units uncolonedDefault, Units colonedDefault, out Dictionary<string, TimeSpan?> matches) {
-            var options = new TimeSpanParserOptions();
-            options.UncolonedDefault = uncolonedDefault;
-            options.ColonedDefault = colonedDefault;
-            return TryParsePrefixed(text, prefixes, options, out matches);
-        }
-        public static bool TryParsePrefixed(string text, string[] prefixes, out Dictionary<string, TimeSpan?> matches) {
-            return TryParsePrefixed(text, prefixes, null, out matches);
-        }
-
-        public static bool TryParsePrefixed(string text, string[] prefixes, TimeSpanParserOptions options, out Dictionary<string, TimeSpan?> matches) {
-            //string[] prefixes = new string[] { "for", "in", "delay", "wait" };
-
-            //TODO: rename "prefixes" to "parameter names" or "keys" or something
-
-            if (options == null) {
-                options = new TimeSpanParserOptions();
-            }
-
-            matches = new Dictionary<string, TimeSpan?>();
-
-
-            //e.g. string pattern = @"\b(for|in|delay|now|wait)\b"; 
-            //TODO: replace spaces with any amount of whitespace (currently @"\ ") e.g. "[\s.']*" (spaces dots or ' ) // perhaps do replacements first to make it easier, e.g. replace "
-            StringBuilder pattern = new StringBuilder();
-            pattern.Append(@"\b("); // must be in (brackets) to be included in results of regex split
-            //pattern.Append(@"?<keyword>"); // name group
-            pattern.Append(string.Join("|", prefixes.Select(prefix => Regex.Escape(prefix))));
-            pattern.Append(@")\b");
-
-            var regex = new Regex(pattern.ToString(), RegexOptions.IgnoreCase);
-            string[] parts = regex.Split(text);
-            //Console.WriteLine("pattern: " + pattern.ToString());
-            int nonkeywordCounter = 0;
-            string currentPrefix = null;
-
-            try {
-                for (int i = 0; i < parts.Length; i++) {
-                    var part = parts[i];
-                    var lc = part.ToLowerInvariant();
-                    if (prefixes.Contains(lc)) {
-                        matches[lc] = null;
-                        currentPrefix = lc;
-
-                    } else {
-                        if (DoParseMutliple(part, out TimeSpan[] timespans, options)) {
-                            for (int j = 0; j < timespans.Length; j++) {
-                                string prefix = currentPrefix ?? nonkeywordCounter++.ToString();
-
-                                matches[prefix] = timespans[j];
-                                currentPrefix = null;
-                            }
-
-                        } else {
-                            if (options.FailOnUnitlessNumber)
-                                return false;
-                        }
-                    }
-                }
-            } catch (ArgumentException e) {
-                //matches = null;
-                //Console.WriteLine(e);
-                if (options.FailOnUnitlessNumber)
-                    return false;
-            }
-
-
-            return true; //return (matches.Count > 0);
-        }
     }
 }
---
+++ TimeParser/TimeParser/TimeSpanParser_Prefixed.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Globalization;
+
+namespace TimeSpanParserUtil {
+    public partial class TimeSpanParser {
+
+        /// <summary>
+        /// Note: a special entries matches["0"] matches["1"] etc are included if `text` starts with timespans.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="uncolonedDefault"></param>
+        /// <param name="colonedDefault"></param>
+        /// <param name="prefixes"></param>
+        /// <param name="matches"></param>
+        /// <returns></returns>
+        public static bool TryParsePrefixed(string text, string[] prefixes, Units uncolonedDefault, Units colonedDefault, out Dictionary<string, TimeSpan?> matches) {
+            var options = new TimeSpanParserOptions();
+            options.UncolonedDefault = uncolonedDefault;
+            options.ColonedDefault = colonedDefault;
+            return TryParsePrefixed(text, prefixes, options, out matches);
+        }
+        public static bool TryParsePrefixed(string text, string[] prefixes, out Dictionary<string, TimeSpan?> matches) {
+            return TryParsePrefixed(text, prefixes, null, out matches);
+        }
+
+        public static bool TryParsePrefixed(string text, string[] prefixes, TimeSpanParserOptions options, out Dictionary<string, TimeSpan?> matches) {
+            //string[] prefixes = new string[] { "for", "in", "delay", "wait" };
+
+            //TODO: rename "prefixes" to "parameter names" or "keys" or something
+
+            if (options == null) {
+                options = new TimeSpanParserOptions();
+            }
+
+            matches = new Dictionary<string, TimeSpan?>();
+
+
+            //e.g. string pattern = @"\b(for|in|delay|now|wait)\b"; 
+            //TODO: replace spaces with any amount of whitespace (currently @"\ ") e.g. "[\s.']*" (spaces dots or ' ) // perhaps do replacements first to make it easier, e.g. replace "
+            StringBuilder pattern = new StringBuilder();
+            pattern.Append(@"\b("); // must be in (brackets) to be included in results of regex split
+            //pattern.Append(@"?<keyword>"); // name group
+            pattern.Append(string.Join("|", prefixes.Select(prefix => Regex.Escape(prefix))));
+            pattern.Append(@")\b");
+
+            var regex = new Regex(pattern.ToString(), RegexOptions.IgnoreCase);
+            string[] parts = regex.Split(text);
+            //Console.WriteLine("pattern: " + pattern.ToString());
+            int nonkeywordCounter = 0;
+            string currentPrefix = null;
+
+            try {
+                for (int i = 0; i < parts.Length; i++) {
+                    var part = parts[i];
+                    var lc = part.ToLowerInvariant();
+                    if (prefixes.Contains(lc)) {
+                        matches[lc] = null;
+                        currentPrefix = lc;
+
+                    } else {
+                        if (DoParseMutliple(part, out TimeSpan[] timespans, options)) {
+                            for (int j = 0; j < timespans.Length; j++) {
+                                string prefix = currentPrefix ?? nonkeywordCounter++.ToString();
+
+                                matches[prefix] = timespans[j];
+                                currentPrefix = null;
+                            }
+
+                        } else {
+                            if (options.FailOnUnitlessNumber)
+                                return false;
+                        }
+                    }
+                }
+            } catch (ArgumentException e) {
+                //matches = null;
+                //Console.WriteLine(e);
+                if (options.FailOnUnitlessNumber)
+                    return false;
+            }
+
+
+            return true; //return (matches.Count > 0);
+        }
+    }
+}
